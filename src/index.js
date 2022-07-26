0ec10322625bf9cf5c9d819edc47cbb96fb120a1
Alen Valek <valekalen@gmail.com>
2022-07-26 13:10:04 +0200

    Backend heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://moodonja.herokuapp.com/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
